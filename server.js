e7033c3d591bd275df27ef0deb2ba6b104ff6e70
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-21 23:14:38 +0000

    port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3306;
 
 app.use(cors());
 app.use(bodyParser.json());
