b4711168b15f932810fbc1e3e093d899aba41e32
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-21 02:29:14 +0000

    push

--- app.js
+++ app.js
@@ -1,16 +1,2 @@
-// Select the movie collection element
-const movieCollection = document.getElementById('movieCollection');
-
-// To track if a movie is being edited
-let editIndex = null; 
-
-// Fetch movies from the backend
-async function fetchMovies() {
-  try {
-    const response = await fetch('/movies'); // API call to get all movies
-    const data = await response.json();
-    displayMovies(data); // Display the movies fetched from the backend
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-  }
-}
+//changing to get data via api
+const API_URL = 'http://localhost:3000/movies';
