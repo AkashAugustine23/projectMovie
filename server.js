0982ee2306c79f189cc62434d6532b4cc2b30af1
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-21 01:45:34 +0000

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3303;
+const port = 3000;
 
 //connecting to my databes in workbench
 const db = mysql.createConnection({
