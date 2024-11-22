20176c87bcd034cb7b78e4ca675d99115286852f
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-22 02:22:10 +0000

    push

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 //changing to get data via api
 const API_URL = 'http://localhost:3000/movies';
+
+async function fetchMovies() {
+    const response = await fetch(API_URL);
+    const movies = await response.json();
+    const movieList = document.getElementById('movie-list');
+    movieList.innerHTML = '';
