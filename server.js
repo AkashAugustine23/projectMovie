7f69665f68fe394783b6fb30ad73acdc55999c2e
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-12-10 22:35:56 +0000

    chnaged process hirearchy and added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cors()); // Allow cross origin request
 app.use(bodyParser.json());
 
 // MySQL connection using pool
@@ -27,31 +29,22 @@ db.getConnection((err, connection) => {
     connection.release(); // Releasing connection back to pool
 });
 
-// Routes
+// Create a movie
+app.post('/movies', (req, res) => {
+    const { title, director, release_year, rating } = req.body;
+    const sql = 'INSERT INTO movies (title, director, release_year, rating) VALUES (?, ?, ?, ?)';
+    db.query(sql, [title, director, release_year, rating], (err, result) => {
+        res.send({ message: 'Movie added!', movieId: result.insertId });
+    });
+});
 
 // Read all movies from db
 app.get('/movies', (req, res) => {
     const sql = 'SELECT * FROM movies';
     db.query(sql, (err, results) => {
-        if (err) {
-            return res.status(500).send({ error: 'Failed to fetch movies' });
-        }
         res.send(results);
     });
 });
-
-// Create a movie (add movie)
-app.post('/movies', (req, res) => {
-    const { title, director, release_year, rating } = req.body;
-    const sql = 'INSERT INTO movies (title, director, release_year, rating) VALUES (?, ?, ?, ?)';
-    db.query(sql, [title, director, release_year, rating], (err, result) => {
-        if (err) {
-            return res.status(500).send({ error: 'Failed to add movie' });
-        }
-        res.send({ message: 'Movie added!', movieId: result.insertId });
-    });
-});
-
 // Start server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
